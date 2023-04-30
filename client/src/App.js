80c216c172261f2ae596f8735ebeab2d58b953ae
Heather Miller <sublet_tunnel.0g@icloud.com>
2023-04-30 13:32:00 -0500

    Added Create and Read Database Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,54 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-// Test funciton for fetching data from the server
-const fetchData = async () => {
+// EXAMPLES FOR CREATING AND READING FROM DATABASE
+// // Test function for Creation
+// const TestCreate = async () => {
+//   const document = { initials: "JDD", time: 1200 };
+//   // Create a new document
+//   try {
+//     const response = await axios.post(
+//       "http://localhost:8000/database/create",
+//       document,
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error("Error fetching data: ", error);
+//   }
+// };
+
+// Test funtion for finding if document exists
+const TestRead = async () => {
+  // Find document
   try {
-    const response = await axios.get('http://localhost:8000/');
+    const response = await axios.get("http://localhost:8000/database/leaderboard");
     console.log(response.data);
   } catch (error) {
     console.error("Error fetching data: ", error);
   }
-}
+};
 
 function App() {
   useEffect(() => {
-    fetchData();
+    TestRead();
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div classinitials="App">
+      <header classinitials="App-header">
+        <img src={logo} classinitials="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          classinitials="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- server/index.js
+++ server/index.js
@@ -2,46 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
+const databaseRouter = require('./routes/database');
 const app = express();
 const PORT = process.env.PORT || 8000;
-const URI = process.env.MONGODB_URI;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-
-// Connect to MongoDB
-const client = new MongoClient(URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to ther server
-    await client.connect();
-    //Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB."
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-// End of MongoDB connection
-
-// Test route to confirm connection to MongoDB
-app.get("/test", (req, res) => {
-  res.send("Hello World from test route!");
-});
+app.use('/database', databaseRouter)
 
 // Routes
 app.get("/", (req, res) => {
