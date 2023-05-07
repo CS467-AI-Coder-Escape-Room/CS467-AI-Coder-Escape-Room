87db4401c9efad54588b287f40c6d0cc1e860ed4
Heather Miller <sublet_tunnel.0g@icloud.com>
2023-05-06 19:14:34 -0500

    Moved Canvas component into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import EscapeRoom from './components/EscapeRoom/EscapeRoom';
+import reportWebVitals from './reportWebVitals';
 
 // Test funciton for fetching data from the server
 const fetchData = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/');
+    const response = await axios.get("https://ai-coder-server.superindependentmedia.com/database/leaderboard");
     console.log(response.data);
   } catch (error) {
     console.error("Error fetching data: ", error);
@@ -19,11 +23,30 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {/* <RoomScene /> */}
-    </div>
+    <Canvas
+    className="webgl"
+    onCreated={({ gl }) => {
+      gl.shadowMap.enabled = true
+      gl.shadowMap.type = THREE.PCFSoftShadowMap
+    }}
+    camera={{
+      position: [-3, 6, 3],
+      rotation: [0, -1, 0],
+      fov: 75,
+      near: 0.1,
+      far: 50
+    }}
+    resize={{ scroll: false }}
+  >
+    <EscapeRoom />
+  </Canvas>
   );
 }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- client/src/index.js
+++ client/src/index.js
@@ -2,37 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as THREE from 'three'
-import { Canvas } from '@react-three/fiber'
-import EscapeRoom from './EscapeRoom';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Canvas
-      className="webgl"
-      onCreated={({ gl }) => {
-        gl.shadowMap.enabled = true
-        gl.shadowMap.type = THREE.PCFSoftShadowMap
-      }}
-      camera={{
-        position: [-3, 6, 3],
-        rotation: [0, -1, 0],
-        fov: 75,
-        near: 0.1,
-        far: 50
-      }}
-      resize={{ scroll: false }}
-    >
-      <EscapeRoom />
-    </Canvas>
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
