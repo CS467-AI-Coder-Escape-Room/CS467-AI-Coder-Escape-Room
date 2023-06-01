5bea4845ac0d3bf1cfa0bf460231b94e7f98c47a
Heather Miller <sublet_tunnel.0g@icloud.com>
2023-05-31 21:39:08 -0500

    Update server file

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log(`Server is listening on port ${port}`);
